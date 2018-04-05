a5a4828595066b4ff91422905920c6a09a9dd7d2
Ben Jackson <b@benjackson.email>
2018-04-05 15:38:57 +1000

    update

--- opt/resource/common.py
+++ opt/resource/common.py
@@ -7,7 +7,6 @@ def get_payload():
     payload = json.load(sys.stdin)
     _, fname = tempfile.mkstemp()
     print("Logging payload to {}".format(fname), file=sys.stderr)
-    print(sys.stdin)
     with open(fname, 'w') as fp:
         fp.write(json.dumps(payload))
     return payload
